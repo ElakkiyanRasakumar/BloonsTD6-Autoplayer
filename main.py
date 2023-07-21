50ea352b1e2fbc8106232bcbd68008bb1fcf5260
Elakkiyan <elakkiyan2006@gmail.com>
2023-07-20 21:12:23 -0400

    Open map selector added to sequence

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pyautogui as pyauto
-from time import *
+import time
+import cv2 as cv
 
-sleep(2)
-start_game_button = pyauto.locateOnScreen('Photos/start_game_button.png')
-pyauto.click(start_game_button)
+# Sleep so I can switch to the game
+time.sleep(3)
+
+
+
+#Start BloonsTD6
+start_game_button = pyauto.locateOnScreen('Photos/start_game_button.png', confidence=0.7)
+pyauto.leftClick(start_game_button)
+
+time.sleep(5)
+
+#Open map selector
+open_map_selector_button = pyauto.locateOnScreen('Photos/open_map_selector_button.png', confidence=0.7)
+pyauto.leftClick(open_map_selector_button)
